81d22da0f7030c22ec2ecada8d0792cbc6f2b015
1aperez <42749606+1aperez@users.noreply.github.com>
2019-03-07 13:54:30 -0600

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,39 @@
+function green25(){
+const doc= document;
+
+
+}
+
+
+function green50(){
+
+
+}
+
+
+function green75(){
+
+
+}
+
+
+function green100(){
+
+
+}
+
+
+function smallToggle(){
+
+
+}
+
+
+function mediumToggle(){
+
+}
+
+
+function largeToggle(){
+
+}
