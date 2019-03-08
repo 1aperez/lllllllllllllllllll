06b74e0a1d95c04720430716d71cc2bcf1d1b516
1aperez <42749606+1aperez@users.noreply.github.com>
2019-03-08 13:57:17 -0600

    Add files via upload

--- toggleFunctions.js
+++ toggleFunctions.js
@@ -1,30 +1,37 @@
 function green25(){
 const doc= document;
-
+let body= doc.querySelector("body");
+body.classList.add("green25");
 
 }
 
 
 function green50(){
-
-
+    const doc= document;
+    let body= doc.querySelector("body");
+    body.classList.add("green50");
+    
 }
 
 
 function green75(){
-
+    const doc= document;
+    let body= doc.querySelector("body");
+    body.classList.add("green75");
 
 }
 
 
 function green100(){
-
+    const doc= document;
+    let body= doc.querySelector("body");
+    body.classList.add("green100");
 
 }
 
 
 function smallToggle(){
-
+section.classList.add("quarter_box")
 
 }
 
